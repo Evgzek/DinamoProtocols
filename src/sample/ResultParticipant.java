5faa9fcd964c72c0c2117504fc20dea39a3673a2
Evgzek <zekamusijhcuk@gmail.com>
2022-10-14 11:04:48 +1000

    handled errors with empty input lines

--- src/sample/ResultParticipant.java
+++ src/sample/ResultParticipant.java
@@ -54,64 +54,97 @@ public class ResultParticipant {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Error");
             alert.setHeaderText(null);
-            alert.setContentText("Не колличество минут!");
+            alert.setContentText("Не указано колличество минут!");
             alert.showAndWait();
         }else if (resultSeconds.getText().equals("")){
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Error");
             alert.setHeaderText(null);
-            alert.setContentText("Не колличество секунд!");
+            alert.setContentText("Не указано колличество секунд!");
+            alert.showAndWait();
+        }else if (resultTens.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Не указано колличество сотых!");
+            alert.showAndWait();
+        }else {
+            DatabaseHandler db = new DatabaseHandler();
+            double number = Integer.parseInt(enterNumberParticipant.getText());
+            int j = Integer.parseInt(resultTens.getText());
+            if (j < 5){
+                j = 0;
+            }else if (j < 10){
+                j = 10;
+            }
+            db.addResultParticipant(Integer.parseInt(resultMinute.getText()), Integer.parseInt(resultSeconds.getText()),
+                    j, number);
+            double h = Integer.parseInt(resultTens.getText());
+            double time = (Integer.parseInt(resultMinute.getText()) * 60) + Integer.parseInt(resultSeconds.getText()) + h/100;
+            System.out.println(time);
+            db.addResultKefParticipant(number, time);
+            enterNumberParticipant.clear();
+            resultTens.clear();
+            resultMinute.clear();
+            resultSeconds.clear();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText(null);
+            alert.setContentText("Результат участника успешно добавлен");
             alert.showAndWait();
         }
-        DatabaseHandler db = new DatabaseHandler();
-        double number = Integer.parseInt(enterNumberParticipant.getText());
-        int j = Integer.parseInt(resultTens.getText());
-        if (j < 5){
-            j = 0;
-        }else if (j < 10){
-            j = 10;
-        }
-        db.addResultParticipant(Integer.parseInt(resultMinute.getText()), Integer.parseInt(resultSeconds.getText()),
-                j, number);
-        double h = Integer.parseInt(resultTens.getText());
-        double time = (Integer.parseInt(resultMinute.getText()) * 60) + Integer.parseInt(resultSeconds.getText()) + h/100;
-        System.out.println(time);
-        db.addResultKefParticipant(number, time);
-        enterNumberParticipant.clear();
-        resultTens.clear();
-        resultMinute.clear();
-        resultSeconds.clear();
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Success");
-        alert.setHeaderText(null);
-        alert.setContentText("Результат участника успешно добавлен");
-        alert.showAndWait();
     }
 
     @FXML
     public void chargeResultParticipant() throws SQLException, ClassNotFoundException{
-        DatabaseHandler db = new DatabaseHandler();
-        double number = Integer.parseInt(enterNumberParticipant.getText());
-        int j = Integer.parseInt(resultTens.getText());
-        if (j < 5){
-            j = 0;
-        }else if (j < 10){
-            j = 10;
+        if (enterNumberParticipant.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Не указан номер участника!");
+            alert.showAndWait();
+        }else if (resultMinute.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Не указано колличество минут!");
+            alert.showAndWait();
+        }else if (resultSeconds.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Не указано колличество секунд!");
+            alert.showAndWait();
+        }else if (resultTens.getText().equals("")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Не указано колличество сотых!");
+            alert.showAndWait();
+        }else{
+            DatabaseHandler db = new DatabaseHandler();
+            double number = Integer.parseInt(enterNumberParticipant.getText());
+            int j = Integer.parseInt(resultTens.getText());
+            if (j < 5){
+                j = 0;
+            }else if (j < 10){
+                j = 10;
+            }
+            db.addResultParticipant(Integer.parseInt(resultMinute.getText()), Integer.parseInt(resultSeconds.getText()),
+                    j, number);
+            double h = Integer.parseInt(resultTens.getText());
+            double time = (Integer.parseInt(resultMinute.getText()) * 60) + Integer.parseInt(resultSeconds.getText()) + h/100;
+            db.addResultKefParticipant(number, time);
+            enterNumberParticipant.clear();
+            resultTens.clear();
+            resultMinute.clear();
+            resultSeconds.clear();
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText(null);
+            alert.setContentText("Результат участника успешно изменен");
+            alert.showAndWait();
         }
-        db.addResultParticipant(Integer.parseInt(resultMinute.getText()), Integer.parseInt(resultSeconds.getText()),
-                j, number);
-        double h = Integer.parseInt(resultTens.getText());
-        double time = (Integer.parseInt(resultMinute.getText()) * 60) + Integer.parseInt(resultSeconds.getText()) + h/100;
-        db.addResultKefParticipant(number, time);
-        enterNumberParticipant.clear();
-        resultTens.clear();
-        resultMinute.clear();
-        resultSeconds.clear();
-        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Success");
-        alert.setHeaderText(null);
-        alert.setContentText("Результат участника успешно изменен");
-        alert.showAndWait();
     }
 }
 
